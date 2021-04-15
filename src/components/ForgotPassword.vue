20b1332d5ee2873a2af58f2a81254a594b2ea66b
mmcmahon321 <70082951+mmcmahon321@users.noreply.github.com>
2021-04-15 20:32:52 +0900

    1

--- src/components/ForgotPassword.vue
+++ src/components/ForgotPassword.vue
@@ -1,12 +1,4 @@
 <template>
-  <v-container grid-list-md>
-    <v-row align="center" justify="center">
-      <v-col cols="12" sm="6" md="4" lg="4">
-        <v-card class="mx-auto elevation-6">
-          <v-card-title>
-            <span align="center" class="headline">Welcome to SubAlert</span>
-          </v-card-title>
-
   <form @submit.prevent="handleSubmit">
       <h3> Forgot Password</h3>
 
@@ -20,15 +12,7 @@
       <button class="btn btn-primary btn-block">Submit</button>
 
   </form>
-
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
 </template>
-
 <script>
 import axios from 'axios'
 
